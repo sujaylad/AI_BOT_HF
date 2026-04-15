4c812b1e1c6435ec95c30d5f369e3cc24490875a
sujaylad <sujayulad@gmail.com>
2026-04-15 15:06:55 +0530

    Update App.py

--- App.py
+++ App.py
@@ -10,11 +10,10 @@ long_text = st.text_area("Enter text to summarizer:",height=200)
 max_length = st.slider("Max Summary Length",min_value=50,max_value=300,value=130)
 min_length = st.slider("Min Summary Length", min_value=20,max_valse=100,value=30)
 if st.button("Summarize"):
-if long_text.strip():
-with st.spinner("Generating summary... "):
-summary = summarizer(long_text, max_length=max_length,
-min_length=min_length, do_sample=False)
-st.subheader(" Summary:")
-st.success(summary[0]['summary_text'])
-else:
-st.warning(" Please enter some text to summarize.")
+  if long_text.strip():
+    with st.spinner("Generating summary... "):
+      summary = summarizer(long_text, max_length=max_length,min_length=min_length, do_sample=False)
+      st.subheader(" Summary:")
+      st.success(summary[0]['summary_text'])
+  else:
+    st.warning(" Please enter some text to summarize.")
